9210bae66b340b44ece95c09e5ad4f59aaef89ea
H.Chihoon <to@ihooni.com>
2019-10-03 16:39:03 +0900

    Update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv'
 import DBServiceProvider from 'Providers/DBServiceProvider'
+import RouteServiceProvider from 'Providers/RouteServiceProvider'
 
 async function bootApp(): Promise<void> {
   await DBServiceProvider.boot()
+  const app = RouteServiceProvider.boot()
+  app.listen(process.env.APP_PORT)
 }
 
 dotenv.config()
