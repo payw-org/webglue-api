d041d2fa61c796c22e92942060af1d2344b90810
H.Chihoon <to@ihooni.com>
2019-10-10 02:48:06 +0900

    Create end point about me(current logged in user)

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import oauth2Router from 'Routes/oauth2'
+import meRouter from 'Routes/me'
 
 const mainRouter = express.Router()
 
@@ -7,6 +8,21 @@ const mainRouter = express.Router()
  * Sub router
  */
 mainRouter.use('/oauth2', oauth2Router)
+mainRouter.use('/me', meRouter)
+
+/**
+ * Controller
+ */
+mainRouter.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json({
+      user: req.user
+    })
+  } else {
+    res.json({
+      test: 'dfdf'
+    })
+  }
 })
 
 export default mainRouter
