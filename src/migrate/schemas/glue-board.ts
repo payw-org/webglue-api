2ca27284d0eb74a1c072adc5bff9effa406de632
ihooni <to@ihooni.com>
2019-11-03 23:30:16 +0900

    Update glueboard schema
    # delete next field
    # create new primary key: id

--- src/migrate/schemas/glue-board.ts
+++ src/migrate/schemas/glue-board.ts
@@ -3,15 +3,15 @@ import { FragmentDoc } from '@@/migrate/schemas/fragment'
 import categorySchema, { CategoryDoc } from '@@/migrate/schemas/category'
 
 export interface GlueBoardDoc extends Document {
+  id: string
   category: CategoryDoc
   fragments?: Array<string | FragmentDoc>
-  next?: string | GlueBoardDoc
 }
 
 const glueBoardSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
   category: { type: categorySchema, required: true },
-  fragments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Fragment' }],
-  next: { type: mongoose.Schema.Types.ObjectId, ref: 'GlueBoard' }
+  fragments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Fragment' }]
 })
 
 export default glueBoardSchema
