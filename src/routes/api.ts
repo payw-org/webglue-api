d9a42aa2e98821f8243dd5c483003be13568edaf
ihooni <to@ihooni.com>
2019-11-10 22:57:25 +0900

    Delete useless api endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -13,19 +13,4 @@ mainRouter.use('/oauth2', oauth2Router)
 mainRouter.use('/me', meRouter)
 mainRouter.use('/mirroring', mirroringRouter)
 
-/**
- * Controller
- */
-mainRouter.get('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.json({
-      text: 'hello ' + (req.user as UserDoc).nickname
-    })
-  } else {
-    res.json({
-      test: 'u r not user'
-    })
-  }
-})
-
 export default mainRouter
