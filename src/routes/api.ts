3154a44ef44234fcb393c87d0c534bd7b04142e9
ihooni <to@ihooni.com>
2019-11-16 03:03:37 +0900

    Use check sharing middleware in the endpoints

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,12 +2,27 @@ import express from 'express'
 import oauth2Router from '@@/routes/oauth2'
 import meRouter from '@@/routes/me'
 import mirroringRouter from '@@/routes/mirroring'
+import CheckGlueBoard from '@/http/middleware/CheckGlueBoard'
+import RequestValidationError from '@/http/middleware/RequestValidationError'
+import CheckSharing from '@/http/middleware/CheckSharing'
 
 const mainRouter = express.Router()
 
 /**
+ * Middleware
+ */
+
+mainRouter.use(
+  '/sharing/:glueboard',
+  CheckGlueBoard.validate(),
+  RequestValidationError.handler(),
+  CheckSharing.handler()
+)
+
+/**
  * Sub router
  */
+
 mainRouter.use('/oauth2', oauth2Router)
 mainRouter.use('/me', meRouter)
 mainRouter.use('/mirroring', mirroringRouter)
