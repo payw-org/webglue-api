792238cce8cb3c43ca15fe4ffeddf28cee765e90
ihooni <to@ihooni.com>
2019-11-15 22:28:03 +0900

    Edit GlueBoard schema
    # Add user field which is GlueBoard owner
    # Add new composite unique key: user and category name

--- src/migrate/schemas/glue-board.ts
+++ src/migrate/schemas/glue-board.ts
@@ -1,17 +1,22 @@
 import mongoose, { Document } from 'mongoose'
 import { FragmentDoc } from '@@/migrate/schemas/fragment'
 import categorySchema, { CategoryDoc } from '@@/migrate/schemas/category'
+import { UserDoc } from '@@/migrate/schemas/user'
 
 export interface GlueBoardDoc extends Document {
+  user: string | UserDoc
   id: string
   category: CategoryDoc
   fragments?: Array<string | FragmentDoc>
 }
 
 const glueBoardSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   id: { type: String, required: true, unique: true },
   category: { type: categorySchema, required: true },
   fragments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Fragment' }]
 })
+
+glueBoardSchema.index({ user: 1, 'category.name': 1 }, { unique: true })
 
 export default glueBoardSchema
