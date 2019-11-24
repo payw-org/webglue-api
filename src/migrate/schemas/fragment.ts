4dde58a6d0828db6fea8aed4024d1c385d3ed2c4
ihooni <to@ihooni.com>
2019-11-24 22:11:02 +0900

    chore: Add new fields to fragment schema for notifiying

--- src/migrate/schemas/fragment.ts
+++ src/migrate/schemas/fragment.ts
@@ -7,6 +7,10 @@ export interface FragmentDoc extends Document {
   xPos: number
   yPos: number
   scale: number
+  subscription: boolean
+  snapshot: string
+  watchCycle: number
+  lastWatchedAt: Date
 }
 
 const fragmentSchema = new mongoose.Schema({
@@ -15,7 +19,11 @@ const fragmentSchema = new mongoose.Schema({
   selector: { type: String, required: true },
   xPos: { type: Number, required: true },
   yPos: { type: Number, required: true },
-  scale: { type: Number, required: true, default: 1 }
+  scale: { type: Number, required: true, default: 1 },
+  subscription: { type: Boolean, required: true, default: false },
+  snapshot: { type: String, required: false },
+  watchCycle: { type: Number, required: false },
+  lastWatchedAt: { type: Date, required: false }
 })
 
 export default fragmentSchema
