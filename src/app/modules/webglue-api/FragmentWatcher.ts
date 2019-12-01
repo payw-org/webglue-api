ecb2e262cd70fa8ce6a345fb8b52b67dc6b950da
ihooni <to@ihooni.com>
2019-12-02 02:57:03 +0900

    feat: get all fragments to watch

--- src/app/modules/webglue-api/FragmentWatcher.ts
+++ src/app/modules/webglue-api/FragmentWatcher.ts
@@ -1,3 +1,7 @@
+import { FragmentDoc } from '@@/migrate/schemas/fragment'
+import Fragment from '@@/migrate/models/fragment'
+import moment from 'moment'
+
 export default class FragmentWatcher {
   private static _instance: FragmentWatcher
 
@@ -19,4 +23,35 @@ export default class FragmentWatcher {
    */
   // eslint-disable-next-line no-useless-constructor,@typescript-eslint/no-empty-function
   private constructor() {}
+
+  private async getAllFragmentsToWatch(): Promise<FragmentDoc[]> {
+    // get all subscribers
+    const subscribers = await Fragment.find(
+      { subscription: true },
+      { _id: 1, watchCycle: 1, lastWatchedAt: 1 }
+    ).lean()
+
+    // filter subscribers which are time to watch
+    const watchList = subscribers.filter((subscriber: FragmentDoc) => {
+      return (
+        subscriber.lastWatchedAt <=
+        moment()
+          .subtract(subscriber.watchCycle, 'minute')
+          .toDate()
+      )
+    })
+
+    return Fragment.find(
+      {
+        _id: { $in: watchList.map(subscriber => subscriber._id) }
+      },
+      {
+        url: 1,
+        selector: 1,
+        headers: 1,
+        snapshot: 1,
+        lastWatchedAt: 1
+      }
+    )
+  }
 }
