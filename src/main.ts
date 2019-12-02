e43f0fbbaf8f22b7937e1193cc6106927a3108bf
ihooni <to@ihooni.com>
2019-12-03 01:11:54 +0900

    feat: add schedule service provider
    # Set scheduling for fragment watcher when app booting

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import dotenv from 'dotenv'
 import DBServiceProvider from '@/providers/DBServiceProvider'
 import RouteServiceProvider from '@/providers/RouteServiceProvider'
 import EventServiceProvider from '@/providers/EventServiceProvider'
+import ScheduleServiceProvider from '@/providers/ScheduleServiceProvider'
 
 async function bootApp(): Promise<void> {
   await DBServiceProvider.boot()
   EventServiceProvider.boot()
+  ScheduleServiceProvider.boot()
   const app = RouteServiceProvider.boot()
   app.listen(process.env.APP_PORT)
 }
